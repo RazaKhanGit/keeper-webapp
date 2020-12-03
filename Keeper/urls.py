eb9b3b72e4f48a8db41c8b8e595812738e349929
Raza Khan <mdrazakhan2019@gmail.com>
2020-12-03 11:41:39 +0530

    intial commit

---
+++ Keeper/urls.py
@@ -0,0 +1,25 @@
+"""Keeper URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Noting import views
+
+urlpatterns = [
+    path('', views.temp),
+    path('noting/', views.main),
+    path('addNote/', views.addNote),
+    path('deleteNote/<int:Note_id>/', views.deleteNote)
+]
---
+++ Noting/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Note
+
+def main(request):
+    all_notes = Note.objects.all()
+    return render(request, 'noting.html', 
+    {'all_items': all_notes})# render the Database as a dictionary to be used in templates
+
+def temp(request):
+    return HttpResponse("This is a temporary view for this Page")
+
+def addNote(request):
+    new_item = Note(title=request.POST['title'], desc=request.POST['desc'])# creating the Note
+    new_item.save()# saving the note
+    return HttpResponseRedirect('/noting/')
+
+def deleteNote(request, Note_id):
+    del_item = Note.objects.get(id = Note_id)
+    del_item.delete()
+    return HttpResponseRedirect('/noting/')
